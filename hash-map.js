8e28a53ddf780f8cd4ebaabb2b4b2630efceb2b2
John Moorman <45500240+mojoro@users.noreply.github.com>
2024-11-12 10:40:51 +0100

    Refactor linkedList for keys and finish set and get for map

--- hash-map.js
+++ hash-map.js
@@ -1,9 +1,9 @@
 import { LinkedList } from "./linked-lists.js";
-
 class HashMap {
   constructor() {
     this.loadFactor = 0.75;
     this.buckets = Array(16);
+    this.mapSize = 0;
   }
   hash(key) {
     let hashCode = 0;
@@ -16,33 +16,41 @@ class HashMap {
     return hashCode;
   }
 
-  // currently this doesn't find the keys.
   set(key, value) {
     const hashCode = this.hash(key);
     const index = hashCode % 16;
-    const entry = { [key]: value };
 
     if (index < 0 || index >= this.buckets.length) {
-      throw new Error("Trying to access index out of bound");
+      throw new Error("Trying to access index out of bounds");
     }
 
     if (!this.buckets[index]) {
       const list = new LinkedList();
-      list.append(entry);
+      list.append(key, value);
       this.buckets[index] = list;
+      this.mapSize++;
     } else {
-      this.buckets[index].append(entry);
-    }
-  }
-
-  // currently this doesn't return the key of the node, it just returns the whole linked list.
-  get(key) {
-    for (const list in this.buckets) {
-      if (list.key == key) {
-        return list;
+      const list = this.buckets[index];
+      const keyIndex = list.find(key);
+      if (keyIndex != "Not found") list.insertAt(key, value, keyIndex);
+      else {
+        list.append(key, value);
+        this.mapSize++;
       }
     }
   }
+
+  //  takes one argument as a key and returns the value that is assigned to this key. If a key is not found, return null
+  get(key) {
+    for (const list in this.buckets) {
+      const keyIndex = list.find(key);
+      if (keyIndex != "Not found") {
+        return list.at(keyIndex);
+      }
+    }
+    return null;
+  }
+
   has(key) {}
   remove(key) {}
   length() {}
@@ -64,5 +72,6 @@ test.set("grape", "purple");
 test.set("hat", "black");
 test.set("ice cream", "white");
 test.set("jacket", "blue");
+test.set("kite", "pink");
 test.set("kite", "pink");
 test.set("lion", "golden");
--- linked-lists.js
+++ linked-lists.js
@@ -1,19 +1,75 @@
+/**
+ * Represents a node in the linked list.
+ */
+class Node {
+  /**
+   * Creates a new node.
+   * @param {string|null} key - The key of the node.
+   * @param {*} value - The value of the node.
+   * @param {Node|null} nextNode - The next node in the list.
+   */
+  constructor(key = null, value = null, nextNode = null) {
+    this.key = key;
+    this.value = value;
+    this.nextNode = nextNode;
+  }
+
+  /**
+   * Sets the key of the node.
+   * @param {string} key - The key to set.
+   */
+  setKey(key) {
+    this.key = key;
+  }
+
+  /**
+   * Sets the value of the node.
+   * @param {*} value - The value to set.
+   */
+  setValue(value) {
+    this.value = value;
+  }
+
+  /**
+   * Sets the next node.
+   * @param {Node} nextNode - The next node to set.
+   */
+  setNext(nextNode) {
+    this.nextNode = nextNode;
+  }
+}
+
+/**
+ * Represents a linked list.
+ */
 class LinkedList {
+  /**
+   * Creates a new linked list.
+   */
   constructor() {
     this.headNode = new Node();
   }
 
-  append(value) {
+  /**
+   * Appends a new node to the end of the list.
+   * @param {string} key - The key of the new node.
+   * @param {*} value - The value of the new node.
+   */
+  append(key, value) {
     if (!this.headNode.value) {
+      this.headNode.setKey(key);
       this.headNode.setValue(value);
     } else {
       const lastNode = this.nextUntilNull(this.headNode);
-      lastNode.setNext(new Node());
-      lastNode.nextNode.setValue(value);
+      lastNode.setNext(new Node(key, value));
     }
   }
 
-  // recursive function to find the last node of the list.
+  /**
+   * Recursively finds the last node of the list.
+   * @param {Node} node - The current node.
+   * @returns {Node} The last node.
+   */
   nextUntilNull(node) {
     if (!node.nextNode || !node.nextNode.value) {
       return node;
@@ -22,13 +78,23 @@ class LinkedList {
     }
   }
 
-  prepend(value) {
-    const newHead = new Node();
-    newHead.setValue(value);
+  /**
+   * Prepends a new node to the beginning of the list.
+   * @param {string} key - The key of the new node.
+   * @param {*} value - The value of the new node.
+   */
+  prepend(key, value) {
+    const newHead = new Node(key, value);
     newHead.setNext(this.headNode);
     this.headNode = newHead;
   }
 
+  /**
+   * Calculates the size of the list.
+   * @param {Node} [node=this.headNode] - The current node.
+   * @param {number} [count=0] - The current count.
+   * @returns {number} The size of the list.
+   */
   size(node = this.headNode, count = 0) {
     if (!node.nextNode || !node.nextNode.value) {
       return ++count;
@@ -37,14 +103,28 @@ class LinkedList {
     }
   }
 
+  /**
+   * Gets the value of the head node.
+   * @returns {*} The value of the head node.
+   */
   head() {
     return this.headNode.value;
   }
 
+  /**
+   * Gets the value of the tail node.
+   * @returns {*} The value of the tail node.
+   */
   tail() {
     return this.nextUntilNull(this.headNode).value;
   }
 
+  /**
+   * Gets the value at a specific index.
+   * @param {number} index - The index to retrieve.
+   * @param {Node} [node=this.headNode] - The current node.
+   * @returns {*} The value at the specified index.
+   */
   at(index, node = this.headNode) {
     if (index == 0) {
       return node.value;
@@ -55,6 +135,11 @@ class LinkedList {
     }
   }
 
+  /**
+   * Removes the last node from the list.
+   * @param {Node} [node=this.headNode] - The current node.
+   * @param {Node} [prevNode=""] - The previous node.
+   */
   pop(node = this.headNode, prevNode = "") {
     if (!node.nextNode || !node.nextNode.value) {
       prevNode.setNext(null);
@@ -63,6 +148,12 @@ class LinkedList {
     }
   }
 
+  /**
+   * Checks if the list contains a specific value.
+   * @param {*} value - The value to check.
+   * @param {Node} [node=this.headNode] - The current node.
+   * @returns {boolean} True if the value is found, false otherwise.
+   */
   contains(value, node = this.headNode) {
     if (node.value == value) {
       return true;
@@ -72,40 +163,65 @@ class LinkedList {
     return false;
   }
 
-  find(value, node = this.headNode, index = 0) {
-    if (node.value == value) {
+  /**
+   * Finds the index of a node with a specific key.
+   * @param {string} key - The key to find.
+   * @param {Node} [node=this.headNode] - The current node.
+   * @param {number} [index=0] - The current index.
+   * @returns {number|string} The index of the node, or "Not found" if not found.
+   */
+  find(key, node = this.headNode, index = 0) {
+    if (node.key === key) {
       return index;
     } else if (node.nextNode) {
-      return this.find(value, node.nextNode, index + 1);
+      return this.find(key, node.nextNode, index + 1);
     }
     return "Not found";
   }
 
+  /**
+   * Converts the list to a string representation.
+   * @param {Node} [node=this.headNode] - The current node.
+   * @param {string} [string=""] - The current string.
+   * @returns {string} The string representation of the list.
+   */
   toString(node = this.headNode, string = "") {
     if (!node.nextNode || !node.nextNode.value) {
-      string += `( ${node.value} ) -> null`;
+      string += `( ${node.key}: ${node.value} ) -> null`;
       return string;
     } else if (node) {
-      string += `( ${node.value} ) -> `;
+      string += `( ${node.key}: ${node.value} ) -> `;
       return this.toString(node.nextNode, string);
     }
   }
 
-  insertAt(value, index, node = this.headNode) {
+  /**
+   * Inserts a new node at a specific index.
+   * @param {string} key - The key of the new node.
+   * @param {*} value - The value of the new node.
+   * @param {number} index - The index to insert at.
+   * @param {Node} [node=this.headNode] - The current node.
+   */
+  insertAt(key, value, index, node = this.headNode) {
     if (index == 0) {
-      this.prepend(value);
+      this.prepend(key, value);
     } else if (index == 1 && node.nextNode) {
-      const newNode = new Node();
-      newNode.setValue(value);
+      const newNode = new Node(key, value);
       newNode.setNext(node.nextNode);
       node.setNext(newNode);
     } else if (node.nextNode) {
-      return this.insertAt(value, index - 1, node.nextNode);
+      return this.insertAt(key, value, index - 1, node.nextNode);
     } else if (!node.nextNode || !node.nextNode.value) {
-      this.append(value);
+      this.append(key, value);
     }
   }
 
+  /**
+   * Removes a node at a specific index.
+   * @param {number} index - The index to remove.
+   * @param {Node} [node=this.headNode] - The current node.
+   * @param {Node} [prevNode=""] - The previous node.
+   */
   removeAt(index, node = this.headNode, prevNode = "") {
     if (index == 0) {
       prevNode.setNext(node.nextNode);
@@ -114,21 +230,6 @@ class LinkedList {
     } else if (!node.nextNode || !node.nextNode.value) {
       this.pop();
     }
-  }
-}
-
-class Node {
-  constructor() {
-    this.value = null;
-    this.nextNode = null;
-  }
-
-  setValue(value) {
-    this.value = value;
-  }
-
-  setNext(nextNode) {
-    this.nextNode = nextNode;
   }
 }
 
